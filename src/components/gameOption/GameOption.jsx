a1489fce93256ea2947c91074bb321deff275ee4
Jhon Byron <jhoncolombia66@gmail.com>
2023-04-15 17:46:38 -0300

    criado componente Game

---
+++ src/components/gameOption/GameOption.jsx
@@ -0,0 +1,11 @@
+import styles from './GameOption.module.css'
+
+function GameOption () {
+  return (
+    <div className={styles.GameOption}>
+      
+    </div>
+  )
+}
+
+export default GameOption
--- src/components/gamepage/Gamepage.jsx
+++ src/components/gamepage/Gamepage.jsx
@@ -1,9 +1,12 @@
 import Header from "../header/Header"
 
+import Game from "../game/Game"
+
 function Gamepage () {
   return (
     <div className="container">
       <Header />
+      <Game />
     </div>
   )
 }
