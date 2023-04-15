5ce9ac66ecbe95ebb83d159c736d29e3117bc73c
Jhon Byron <jhoncolombia66@gmail.com>
2023-04-15 18:43:45 -0300

    criado estados no Gameoptions

--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,19 +1,21 @@
+import { useState } from 'react'
 import styles from './Game.module.css'
 
 import GameOption from '../gameOption/GameOption'
 
+
+
 function Game () {
+  const [gameState, setgameState] = useState(Array(9).fill(0))
   return (
     <div className={styles.game}>
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
-      <GameOption />
+      {
+        gameState.map((value, position) =>
+        <GameOption 
+        key={`game-option-pos-${position}`} status={value}/>
+        )
+      }
+
     </div>
   )
 }
--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,18 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+const GameIcon = ({ iconName }) => <Icon iconName={iconName} size="25px" />
+
+function GameOption ({ status }) {
   return (
     <div className={styles.GameOption}>
-      
+      {
+        status === 1 &&       <GameIcon iconName="circle" />
+      }
+      {
+        status === -1 &&       <GameIcon iconName="x" />
+      }
+
     </div>
   )
 }
