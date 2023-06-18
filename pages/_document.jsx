70dcd7c2d0d7bf239efacaf0238e51a0d39d5219
Jhon Byron <jhoncolombia66@gmail.com>
2023-06-18 16:09:39 -0300

    Criado icon para titulo

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,7 @@ class NewDocument extends Document {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+          <link rel="icon" href="./icons/tic-tac-toe-svgrepo-com.svg" />
         </Head>
         <body>
           <Main />
